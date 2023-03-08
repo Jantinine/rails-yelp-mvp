38f6b361b13d8a152752e136c53a640a98d51779
Jantinine <jantinederksen@gmail.com>
2023-03-08 17:13:45 +0100

    Seed added to restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+bandana = {name: "Bandana", address: "Calle 24, Rome", category: "italian"}
+bondono = {name: "Bondono", address: "Calle 12, Tokyo", category: "japanese"}
+bundunu = {name: "Bundunu", address: "Calle 4, Beijing", category: "chinese"}
+bindini = {name: "Bindini", address: "Calle 8, Brugge", category: "belgian"}
+bendene = {name: "Bendene", address: "Calle 41, Bordeaux", category: "french"}
+
+[bandana, bondono, bundunu, bindini, bendene].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
